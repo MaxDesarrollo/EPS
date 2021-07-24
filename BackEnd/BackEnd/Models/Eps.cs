ee12397dd3be81dbbfc1d659dd691338b904931b
MaxDesarrollo <maxdesarrollo@outlook.com>
2021-07-24 19:24:44 -0400

    Initial Load BackEnd
    + Initial Configuration of BackEnd
    + Model Directory
    + Repositories Directory
    + Interface Directorory
    + Service Directory

---
+++ BackEnd/BackEnd/Models/Eps.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Eps
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Codigo { get; set; }
+        public string Entidad { get; set; }
+        public string Nit { get; set; }
+        public string Regimen_Codigo { get; set; }
+        public string Regimen_Descripcion { get; set; }
+    }
+}
---
+++ BackEnd/BackEnd/Repositories/RutasMedicasDBRepository.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Repositories
+{
+    public class RutasMedicasDBRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public RutasMedicasDBRepository()
+        {
+            client = new MongoClient("mongodb+srv://max_admin:Mememe555@cluster0.e9aje.mongodb.net/rutas_medicas?retryWrites=true&w=majority");
+            db = client.GetDatabase("rutas_medicas");
+        }
+
+    }
+}
